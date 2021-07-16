24461c0ac74756a5f141198c32adfd6d7c4d3333
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-07-16 17:21:09 +0200

    Implemented HttpClientFactory

--- Movies.Client/Program.cs
+++ Movies.Client/Program.cs
@@ -52,6 +52,34 @@ namespace Movies.Client
         {
             // add loggers           
             serviceCollection.AddLogging(configure => configure.AddDebug().AddConsole());
+            serviceCollection.AddHttpClient("MoviesClient", client =>
+            {
+                client.BaseAddress = new Uri("http://localhost:33333");
+                client.Timeout = new TimeSpan(0, 0, 30);
+                client.DefaultRequestHeaders.Clear();
+            }).ConfigurePrimaryHttpMessageHandler(handler =>
+                new HttpClientHandler()
+                {
+                    AutomaticDecompression = System.Net.DecompressionMethods.GZip
+                });
+
+            //serviceCollection.AddHttpClient<MoviesClient>(client =>
+            //{
+            //    client.BaseAddress = new Uri("http://localhost:33333");
+            //    client.Timeout = new TimeSpan(0, 0, 30);
+            //    client.DefaultRequestHeaders.Clear();
+            //}).ConfigurePrimaryHttpMessageHandler(handler =>
+            //    new HttpClientHandler()
+            //    {
+            //        AutomaticDecompression = System.Net.DecompressionMethods.GZip
+            //    });
+
+            serviceCollection.AddHttpClient<MoviesClient>().ConfigurePrimaryHttpMessageHandler(handler =>
+                new HttpClientHandler()
+                {
+                    AutomaticDecompression = System.Net.DecompressionMethods.GZip
+                });
+
 
             // register the integration service on our container with a 
             // scoped lifetime
@@ -66,10 +94,10 @@ namespace Movies.Client
             //serviceCollection.AddScoped<IIntegrationService, StreamService>();
 
             // For the cancellation demos
-             serviceCollection.AddScoped<IIntegrationService, CancellationService>();
+            // serviceCollection.AddScoped<IIntegrationService, CancellationService>();
 
             // For the HttpClientFactory demos
-            // serviceCollection.AddScoped<IIntegrationService, HttpClientFactoryInstanceManagementService>();
+            serviceCollection.AddScoped<IIntegrationService, HttpClientFactoryInstanceManagementService>();
 
             // For the dealing with errors and faults demos
             // serviceCollection.AddScoped<IIntegrationService, DealingWithErrorsAndFaultsService>();
--- Movies.Client/Services/HttpClientFactoryInstanceManagementService.cs
+++ Movies.Client/Services/HttpClientFactoryInstanceManagementService.cs
@@ -1,14 +1,123 @@
-﻿using System;
+﻿using Movies.Client.Models;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Movies.Client.Services
 {
     public class HttpClientFactoryInstanceManagementService : IIntegrationService
     {  
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly MoviesClient _moviesClient;
+
+        public HttpClientFactoryInstanceManagementService(IHttpClientFactory httpClientFactory, MoviesClient moviesClient)
+        {
+            _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+            _moviesClient = moviesClient ?? throw new ArgumentNullException(nameof(moviesClient));
+        }
+
         public async Task Run()
         {
+
+            ///await TestDisposeHttpClient(_cancellationTokenSource.Token);
+            //await TestHttpClient(_cancellationTokenSource.Token);
+            // reusing http handlers and responding to the dns changes by introducing HttpClientFactory
+            //await GetMoviesWithHttpClientFromFactory(_cancellationTokenSource.Token);
+            //await GetMoviesWithNamedHttpClientFromFactory(_cancellationTokenSource.Token);
+            //await GetMoviesWithTypedHttpClientFromFactory(_cancellationTokenSource.Token);
+            await GetMoviesWithTypedMoviesClient(_cancellationTokenSource.Token);
+        }
+
+        private async Task GetMoviesWithTypedMoviesClient(CancellationToken cancellationToken)
+        {
+            await _moviesClient.GetMovies(cancellationToken);
+        }
+
+        //private async Task GetMoviesWithTypedHttpClientFromFactory(CancellationToken cancellationToken)
+        //{
+        //    var request = new HttpRequestMessage(HttpMethod.Get, "api/movies");
+        //    request.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        //    request.Headers.AcceptEncoding.Add(new System.Net.Http.Headers.StringWithQualityHeaderValue("gzip"));
+
+        //    using (var response = await _moviesClient.Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
+        //    {
+        //        var stream = await response.Content.ReadAsStreamAsync();
+        //        response.EnsureSuccessStatusCode();
+        //        var movies = stream.ReadAndDeserializeFromJson<List<Movie>>();
+        //    }
+        //}
+
+        private async Task GetMoviesWithNamedHttpClientFromFactory(CancellationToken cancellationToken)
+        {
+            var httpClient = _httpClientFactory.CreateClient("MoviesClient");
+
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/movies");
+            request.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.AcceptEncoding.Add(new System.Net.Http.Headers.StringWithQualityHeaderValue("gzip"));
+
+            using (var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
+            {
+                var stream = await response.Content.ReadAsStreamAsync();
+                response.EnsureSuccessStatusCode();
+                var movies = stream.ReadAndDeserializeFromJson<List<Movie>>();
+            }
+        }
+
+        private async Task GetMoviesWithHttpClientFromFactory(CancellationToken cancellationToken)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:33333/api/movies");
+            request.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            using (var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
+            {
+                var stream = await response.Content.ReadAsStreamAsync();
+                response.EnsureSuccessStatusCode();
+                var movies = stream.ReadAndDeserializeFromJson<List<Movie>>();
+            }
+        }
+
+        private async Task TestDisposeHttpClient(CancellationToken cancellationToken)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Get, "https://www.google.com");
+
+                    using (var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
+                    {
+                        var stream = await response.Content.ReadAsStreamAsync();
+                        response.EnsureSuccessStatusCode();
+
+                        Console.WriteLine($"Request completed with status code: {response.StatusCode}");
+                    }
+                }
+            }
+        }
+
+        private async Task TestHttpClient(CancellationToken cancellationToken)
+        {
+            var httpClient = new HttpClient();
+
+            for (var i = 0; i < 10; i++)
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, "https://www.google.com");
+
+                using (var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
+                {
+                    var stream = await response.Content.ReadAsStreamAsync();
+                    response.EnsureSuccessStatusCode();
+
+                    Console.WriteLine($"Request completed with status code: {response.StatusCode}");
+                }
+            }
         }
     }
 }
