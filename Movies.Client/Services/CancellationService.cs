df27514f7c72fa0bc848795bb209784413afa6fb
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-07-16 10:12:05 +0200

    Handled cancellation and timeouts

--- Movies.Client/Services/CancellationService.cs
+++ Movies.Client/Services/CancellationService.cs
@@ -1,14 +1,80 @@
-﻿using System;
+﻿using Movies.Client.Models;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Movies.Client.Services
 {
     public class CancellationService : IIntegrationService
     {
+        private static HttpClient _httpClient = new HttpClient(
+            new HttpClientHandler()
+            {
+                AutomaticDecompression = System.Net.DecompressionMethods.GZip
+            });
+
+        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        public CancellationService()
+        {
+            _httpClient.BaseAddress = new Uri("http://localhost:33333");
+            _httpClient.Timeout = new TimeSpan(0, 0, 2);
+            _httpClient.DefaultRequestHeaders.Clear();
+        }
+
         public async Task Run()
         {
+            //_cancellationTokenSource.CancelAfter(100);
+            //GetTrailerAndCancel(_cancellationTokenSource.Token);
+            GetTrailerAndHandleTimeout();
+        }
+
+        private async Task GetTrailerAndCancel(CancellationToken cancellationToken)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"api/movies/5b1c2b4d-48c7-402a-80c3-cc796ad49c6b/posters/{Guid.NewGuid()}");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+
+            try
+            {
+                using (var response = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
+                {
+                    var stream = await response.Content.ReadAsStreamAsync();
+                    response.EnsureSuccessStatusCode();
+                    var trailer = stream.ReadAndDeserializeFromJson<Trailer>();
+                }
+            }
+            catch (OperationCanceledException exception)
+            {
+                Console.WriteLine($"An operation was cancelled with message {exception.Message}");
+                // additional cleanup...
+            }
+        }
+
+        private async Task GetTrailerAndHandleTimeout()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"api/movies/5b1c2b4d-48c7-402a-80c3-cc796ad49c6b/posters/{Guid.NewGuid()}");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+
+            try
+            {
+                using (var response = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead))
+                {
+                    var stream = await response.Content.ReadAsStreamAsync();
+                    response.EnsureSuccessStatusCode();
+                    var trailer = stream.ReadAndDeserializeFromJson<Trailer>();
+                }
+            }
+            catch (OperationCanceledException exception)
+            {
+                Console.WriteLine($"An operation was cancelled with message {exception.Message}");
+                // additional cleanup...
+            }
         }
     }
 }
